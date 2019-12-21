17688f57d8028edd46d6fcc65720add8a0c390dc
Andrew Ochkurov <andrey4016@gmail.com>
2019-12-21 23:49:54 +0200

    DataBase : genre repo

--- BookLibrary/BookLibrary.DAL/Repositories/GenreRepository.cs
+++ BookLibrary/BookLibrary.DAL/Repositories/GenreRepository.cs
@@ -1,35 +1,58 @@
-﻿using BookLibrary.DAL.Interfaces;
+﻿using BookLibrary.DAL.DbContext;
+using BookLibrary.DAL.Interfaces;
 using BookLibrary.Domain.Models;
-using System;
+using Dapper;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace BookLibrary.DAL.Repositories
 {
     public class GenreRepository : IGenreRepository
     {
+        private readonly IDbConnection _connection;
+
+        public GenreRepository(LibraryDbContext dbContext)
+        {
+            _connection = dbContext.Connection;
+        }
+
         public void Create(Genre entity)
         {
-            throw new NotImplementedException();
+            var sql = "INSERT [dbo].[genres] ([GenreName]) " +
+                        "VALUES(@GenreName)";
+
+            entity.GenreId = _connection.ExecuteScalar<int>(sql, entity);
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            var sql = "DELETE FROM genres WHERE GenreId = @Id";
+
+            _connection.Execute(sql, new { Id = id });
         }
 
         public void Edit(Genre entity)
         {
-            throw new NotImplementedException();
+            var sql = "UPDATE [dbo].[genres] SET [GenreName] = @GenreName WHERE [GenreId]  = @GenreId";
+
+            _connection.Execute(sql, entity);
         }
 
         public Genre Get(int id)
         {
-            throw new NotImplementedException();
+            var sql = "SELECT * FROM genres WHERE GenreId = @Id";
+            var genre = _connection.Query<Genre>(sql, new { Id = id }).FirstOrDefault();
+
+            return genre;
         }
 
         public IEnumerable<Genre> Get()
         {
-            throw new NotImplementedException();
+            var sql = "SELECT * FROM genres";
+            var genres = _connection.Query<Genre>(sql);
+
+            return genres;
         }
     }
 }
